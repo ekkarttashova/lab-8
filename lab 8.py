b01c9e2746c0d8ada2e49c2d7de84bbea8d0a1e2
Kate Kartashova <yekaterina_kartachova05@mail.ru>
2024-05-08 22:41:59 +0300

    Add files via upload

---
+++ lab 8.py
@@ -0,0 +1,45 @@
+import cv2
+def write_coordinates_to_file(x, y):
+    with open('marker_coordinates.txt', 'a') as file:
+        file.write(f'{x}, {y}\n')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, image = cap.read()
+    if not success:
+        break
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    _, binary = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+
+    #находим контуры
+    contours, _ = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area > 1000:
+            x, y, a, b = cv2.boundingRect(contour)
+            #Рисуем прямоугльник вокруг контура
+            cv2.rectangle(image, (x, y), (x + a, y + b), (255, 0, 0), 2)
+            center_x = x + a // 2
+            center_y = y + b // 2
+            write_coordinates_to_file(center_x, center_y)
+
+
+    cv2.imshow('image with label', image)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Освободить камеру
+cap.release()
+
+# Закрыть все окна
+cv2.destroyAllWindows()
+
+
+
+
+
---
+++ lab_8.py
@@ -0,0 +1,6 @@
+import cv2
+#1 задание
+image = cv2.imread('image-variant2.jpg')
+blurred_image = cv2.GaussianBlur(image, ksize=(11, 11), sigmaX =0, sigmaY=0)
+cv2.imwrite('blurred_image.jpg', blurred_image)
+#2 и 3 задания
