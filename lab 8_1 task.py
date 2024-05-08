7b8c799815b22a4e44f2134e91fcb4a22f717b4b
Kate Kartashova <yekaterina_kartachova05@mail.ru>
2024-05-08 22:42:47 +0300

    lab 8_1 task.py

--- lab 8_1 task.py
+++ lab 8_1 task.py
@@ -3,4 +3,3 @@ import cv2
 image = cv2.imread('image-variant2.jpg')
 blurred_image = cv2.GaussianBlur(image, ksize=(11, 11), sigmaX =0, sigmaY=0)
 cv2.imwrite('blurred_image.jpg', blurred_image)
-#2 и 3 задания
